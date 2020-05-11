ed5bf6e4f4c82ea07cac4060b9ba97d3626d7e8c
sinatra <is.swag@outlook.com>
2020-05-10 19:12:19 -0500

    [ACP-1] Add roadmap repository

---
+++ src/main/java/co/edu/udea/adsii/plataformaeducativa/component/roadmap/io/repository/RoadmapRepository.java
@@ -0,0 +1,8 @@
+package co.edu.udea.adsii.plataformaeducativa.component.roadmap.io.repository;
+
+import co.edu.udea.adsii.plataformaeducativa.component.roadmap.model.Roadmap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
+
+}
